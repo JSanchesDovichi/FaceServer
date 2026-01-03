a5cfe886e7a897777e239e3af2cb5bf2f339def4
João Pedro Sanches Dovichi <jsanchesdovichi@gmail.com>
2026-01-02 22:07:51 -0300

    DB type-casting in endpoints progress

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,10 +1,20 @@
+use rocket::serde::json::Value;
 use std::fs;
 use std::time::{Duration, SystemTime};
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
-use rocket::{get, post, launch, routes, FromForm};
+use rocket::{get, post, launch, routes, FromForm, State};
 use rocket_dyn_templates::{Template, context};
 use rocket::form::Form;
+use rocket::serde::{Deserialize, Serialize, json::Json};
 use rocket::fs::TempFile;
+use rocket::http::ext::IntoCollection;
+use surrealdb::{Error, Surreal};
+use surrealdb::opt::auth::Root;
+//use surrealdb::engine::local::RocksDb;
+use surrealdb::engine::local::{Db, RocksDb};
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::method::Select;
+use surrealdb::types::{SurrealValue};
 
 async fn setup_scheduled_task() -> Result<(), JobSchedulerError> {
     let mut sched = JobScheduler::new().await?;
@@ -34,7 +44,7 @@ struct Upload<'r> {
 }
 
 #[post("/upload_image", data = "<upload>")]
-async fn upload_form(mut upload: Form<Upload<'_>>) {
+async fn upload_form(mut upload: Form<Upload<'_>>, db: &State<Surreal<Db>>) {
     for file in &mut upload.files {
         println!("Received to {:?}", file.path());
 
@@ -49,11 +59,63 @@ async fn upload_form(mut upload: Form<Upload<'_>>) {
     }
 }
 
+#[get("/test_db")]
+async fn test_db(db: &State<Surreal<Db>>) {
+    #[derive(Serialize, Deserialize, Debug, SurrealValue)]
+    #[serde(crate = "rocket::serde")]
+    struct Person {
+        name: String,
+    }
+
+    /*
+    match db.query("CREATE person:tobie SET name = 'Tobie'").await {
+        Ok(rows) => {
+            println!("{:#?}", rows);
+        },
+        Err(e) => {}
+    }
+
+    match db.query("SELECT * FROM person").await {
+        Ok(rows) => {
+            println!("{:#?}", rows);
+        },
+        Err(e) => {}
+    }
+     */
+
+    // Create a new person with a random ID
+    let created: Option<Person> = db.create(("person", "tobie"))
+        .content(Person {
+            name: "Tobie".to_string()
+        })
+        .await.unwrap();
+
+    // Select all people records
+    let people: Vec<Person> = db.select("person").await.unwrap();
+
+    println!("{:?}", people);
+}
+
 #[launch]
 async fn rocket() -> _ {
     if let Err(e) = setup_scheduled_task().await {
         panic!("Failed to setup scheduled task: {:?}", e);
     }
 
-    rocket::build().mount("/", routes![index, upload_form]).attach(Template::fairing())
+    let db = match Surreal::new::<RocksDb>("./target/debug/database").await {
+        Ok(db) => {
+            // Select a specific namespace / database
+            if let Err(e) = db.use_ns("namespace").use_db("database").await {
+                panic!("Failed to determine namespace and database {:?}", e);
+            }
+
+            db
+        },
+        Err(e) => panic!("Failed to setup database: {:?}", e)
+    };
+
+    rocket::build()
+        .mount("/", routes![index, upload_form, test_db])
+        .manage(db)
+        .attach(Template::fairing())
 }
